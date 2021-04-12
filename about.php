6115d70ec47fb9a86a307628af20f00cdb65ebf4
mancmaur <58760554+mancmaur@users.noreply.github.com>
2021-04-12 13:13:03 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,4 @@
+<h1><?php echo $title;?></h1>
+<p>A. P. Shah Institute of Technology (APSIT) has started functioning with commitment of imparting state of art technical education so as to inculcate conceptual know-how, analyzing skills, decision making abilities and leadership qualities in the students. APSIT stands committed to the intellectual and moral growth of every student.
+
+APSIT has experienced and proficient team which aspires to unlock the hidden potential in subconscious minds of students and to create competent Engineers with vision & social commitment.</p>
---
+++ home.php
@@ -0,0 +1,59 @@
+<!-- <h1><?php echo $title;?></h1> -->
+
+<div class="col-lg-12">  
+<h4>Belief</h4>
+    <p> THE PURPOSE OF EDUCATION is to give to the body and the soul all the beauty and all the perfection of which they are capable of and that, the direction in which students are guided to learning will determine the future course of his life.</p> 
+</div>
+<div  class="col-lg-6">
+  <h4>Vision</h4> 
+  <p>Education is a participative and collaborative activity. We , at DAV public primary school, I believe on education as an ongoing process and aim to provide holistic , scientific development hand in hand with academic excellence. Our vision is to enrich young, delicate minds with the ideals of democracy, socialism and secularism. Our values and content based curriculum and activities train not only the body and mind but the spirit as well. Synthesis of modern technology and knowledge along with ancient heritage and culture forms the core of our educative endeavor.</p>
+  <h4>Mission</h4>
+  <p> The AP SHAH School believes in the uniqueness of each and every child and thus creates a very child friendly conductive environment to make learning a fruitful and joyous life long experience. We provide excellence in education and also strive to carve out a well rounded personality of every student. We endeavor to enrich our students with integrity, character, and leadership qualities. Nurturing talent and exploring potential, we promote experiment based activities and scientific temper. We promote quality education and strengthen both body and mind, to face the challenges of tomorrow.
+
+</p>
+  <h4>Goals</h4> 
+  <ol>
+    <li>Globally competitive graduates</li>  
+    <li>Institutionalize research culture</li> 
+    <li>Responsive and sustainable extension services</li> 
+    <li>Maximized profit of variable agro-industrial business ventures</li>  
+    <li>Effective and efficient administration </li>  
+  </ol>
+
+  <h4>Objectives</h4> 
+   <ol>
+      <li>Create an environment of shared leadership and responsibilities with competent administrator.</li>
+      <li>Provide relevant trainings and seminar to faculty, staff, and student.</li>
+      <li>Produce highly competitive graduates.</li>
+      <li>Conduct relevant and updated researches</li>
+      <li>Extend financial support and manpower for outreach activities.</li> 
+   </ol> 
+</div>
+<div  class="col-lg-6">
+  <h4>Guiding Principles</h4> 
+    <ul>
+      <li>Academic Freedom </li>
+      <li>Responsibility</li>
+      <li> Academic Standards</li>  
+    </ul>
+<h4>Core Values</h4>
+  <ul>
+    <li>God-Centered</li>
+    <li>Excellence</li>
+    <li>Integrity</li>
+    <li>Transparency and Accountability</li>
+    <li>Dedication to Quality Service</li>  
+  </ul> 
+<h4>Quality Policy</h4> 
+<ol>
+    <li>AP SHAH is committed to be the center of academic excellence delivering quality service to all by;</li> 
+    <li>Continuing innovations and quality improvement cultivating an efficient and effective and environment for maximum clientele satisfaction;</li> 
+    <li>Adhering to laws and regulations, global standards, and environmental change requirements;</li> 
+    <li>Participating in sustainable developments projects for inclusive economic for inclusive 
+economic growth;</li> 
+    <li>Showcasing quality outputs; and,</li> 
+    <li>Upholding values and integrity and nurturing talents and skills for global competitiveness.</li>  
+</ol>
+</div>
+ 
+
